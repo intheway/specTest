1b5078f6d10f2115baa7d30c673ed11fe3ea15d4
君展 <junzhan.yzw@taobao.com>
2013-03-13 15:47:02 +0800

    add test file

---
+++ GitSpecTest/TestSDK/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  GitSpecTest
+//
+//  Created by junzhan on 3/13/13.
+//  Copyright (c) 2013 君展. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
